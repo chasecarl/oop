9056e8535db0c27279c781c4fcc76f5f57f6a8f5
Andrey Cherkasov <andreychas1@gmail.com>
2018-05-19 21:02:47 +0300

    added comments to Tree.java

--- ex4/data_structures/Tree.java
+++ ex4/data_structures/Tree.java
@@ -2,21 +2,24 @@ package oop.ex4.data_structures;
 
 public class Tree {
 
+    /** The root node of a tree */
     TreeNode root;
 
+    /** Represents a single node of a tree */
     private class TreeNode {
 
+        /** The left child of a tree */
         private TreeNode left;
+        /** The right child of a tree */
         private TreeNode right;
+        /** The value that this node stores */
         private int value;
 
+        /** Constructs a TreeNode given its integer value */
         private TreeNode(int value) { this.value = value; }
 
+        /** Constructs a TreeNode with the default value of 0 */
         private TreeNode() { this(0); }
 
-    }
-
-    public Tree() {
-        this.root = new TreeNode();
     }
 }
