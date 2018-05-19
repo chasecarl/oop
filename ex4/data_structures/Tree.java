3b4a26ef6057355f18896af3192dc6a9a0067c5b
Andrey Cherkasov <andreychas1@gmail.com>
2018-05-19 21:41:42 +0300

    added height field to the TreeNode

--- ex4/data_structures/Tree.java
+++ ex4/data_structures/Tree.java
@@ -4,7 +4,7 @@ public class Tree {
 
     /** The root node of a tree */
     TreeNode root;
-
+    private int height;
     /** Represents a single node of a tree */
     private class TreeNode {
 
@@ -14,12 +14,18 @@ public class Tree {
         private TreeNode right;
         /** The value that this node stores */
         private int value;
+        /** The height of this node */
+        private int height;
 
         /** Constructs a TreeNode given its integer value */
-        private TreeNode(int value) { this.value = value; }
+        private TreeNode(int value) {
+            this.value = value;
+            this.height = 0;
+        }
 
         /** Constructs a TreeNode with the default value of 0 */
         private TreeNode() { this(0); }
-
     }
+
+
 }
