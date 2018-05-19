491341aafa0e76a928c36af09523b11da7ae6aa1
Andrey Cherkasov <andreychas1@gmail.com>
2018-05-19 23:02:55 +0300

    added string representation of the tree, constructors and the height method

--- ex4/data_structures/Test.java
+++ ex4/data_structures/Test.java
@@ -2,7 +2,8 @@ package oop.ex4.data_structures;
 
 public class Test {
     public static void main(String[] args) {
-        Tree tree = new Tree();
+        int[] data = { 0 };
+        Tree tree = new Tree(data);
         System.out.println(tree);
     }
 }
--- ex4/data_structures/Tree.java
+++ ex4/data_structures/Tree.java
@@ -4,7 +4,7 @@ public class Tree {
 
     /** The root node of a tree */
     TreeNode root;
-    private int height;
+
     /** Represents a single node of a tree */
     private class TreeNode {
 
@@ -27,5 +27,63 @@ public class Tree {
         private TreeNode() { this(0); }
     }
 
+    /** @return the height of the tree */
+    private int height() { return root.height; }
+    
+    /** A default constructor */
+    public Tree() { this.root = null; }
 
+    /** A constructor that builds the tree by adding the elements in the input array one-by-one.
+     * If the same values appears twice (or more) in the list, it is ignored. */
+    public Tree(int[] data) {
+        this.root = new TreeNode(data[0]);
+    }
+
+    // TODO: THESE METHODS AREN'T REQUIRED AND NEED TO BE CUT OFF AT THE END
+    private int[] toArray() {
+        int[] result = new int[(int)Math.pow(2, height())];
+
+        traverseToArray(1, result, root);
+
+        return result;
+    }
+
+    private void traverseToArray(int i, int[] result, TreeNode current) {
+        if (current == null) { return; }
+        result[i - 1] = current.value;
+        traverseToArray(i * 2, result, current.left);
+        traverseToArray(i * 2 + 1, result, current.right);
+    }
+
+    public String toString() {
+        int[] arrayTree = toArray();
+        String result = "";
+        int spaceNumber = height();
+
+        String currentHeightString = "";
+        String currentSpaces = "";
+        for (int i = 0; i < arrayTree.length; i++) {
+            if (isPowerOfTwo(i + 1)) {
+                if (spaceNumber != height()) {
+                    currentHeightString += currentSpaces + "\n";
+                    result += currentHeightString;
+                }
+                currentSpaces = getNumberOfSpaces(spaceNumber);
+                spaceNumber--;
+                currentHeightString = currentSpaces;
+            }
+            currentHeightString += arrayTree[i] + " ";
+        }
+        result += currentHeightString.substring(0, currentHeightString.length() - 1);
+
+        return result;
+    }
+
+    private String getNumberOfSpaces(int number) {
+        String result = "";
+        for (int i = 0; i < number; i++) { result += " "; }
+        return result;
+    }
+
+    private boolean isPowerOfTwo(int i) { return i == 1 || i % 2 == 0; }
 }
